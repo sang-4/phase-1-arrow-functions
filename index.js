bfe48497c0bc3716cd4365d4bd4d615b93bcf677
Allan <kipropsallan@gmail.com>
2022-06-11 20:43:14 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+//function expression divide
+const divide = (num1 = 2000, num2 = 100) => num1/num2;
+
+//returns square
+const square = (num = 2) => num**2;
+
+//add values
+const add = (num1 = 3, num2 = 4) => num1 + num2;
